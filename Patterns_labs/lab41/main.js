c9aa5412045e83da9fdf4d20d9f96d41abf7e74b
Bor-is-luv <zverkii5@gmail.com>
2020-03-10 02:05:21 +0300

    try use proxy pattern

--- Patterns_labs/lab41/main.js
+++ Patterns_labs/lab41/main.js
@@ -1,34 +1,56 @@
 const path = require('path');
 const url = require('url');
-const {app, BrowserWindow, nativeImage, globalShortcut} = require('electron');
+const { app, BrowserWindow, nativeImage, globalShortcut } = require('electron');
 
-let win;
+//let win;
 
-function createWindow() {
-    let img = nativeImage.createFromPath("img1.jpg");
-    let size = img.getSize();
-    console.log(size);
-	win = new BrowserWindow({
-		width: size.width,
-		height: size.height//,
-		//icon: __dirname + "/img/icon.png"
-	});
+class AppUI {
+    constructor() { };
 
-	win.loadURL(url.format({
-		pathname: path.join(__dirname, 'main.html'),
-		protocol: 'file:',
-		slashes: true
-	}));
+    drawWindow(pic) {
+        function createWindow() {
+            this.img = nativeImage.createFromPath(pic);
+            let size = this.img.getSize();
+            this.win = new BrowserWindow({
+                width: size.width,
+                height: size.height
+            });
 
-	//win.webContents.();
+            this.win.loadURL(url.format({
+                pathname: path.join(__dirname, 'main.html'),
+                protocol: 'file:',
+                slashes: true
+            }));
 
-	win.on('closed', () => {
-		win = null;
-	});
+            this.win.on('closed', () => {
+                this.win = null;
+            });
+        }
+
+        app.on('ready', createWindow);
+
+        app.on('window-all-closed', () => {
+            app.quit();
+        }
+        )
+    }
+
+    changeBGImage(img){
+        document.addEventListener('mousedown', function(event) {
+            if (event.button == 2) {
+            document.body.style.backgroundImage = "url(" + img + ")";
+    }});
+    }
+
+    changeBGImage1 = (img) => {
+        document.addEventListener('mousedown', function(event) {
+            if (event.button == 2) {
+            document.body.style.backgroundImage = "url(" + img + ")";
+    }});
+    }
 }
 
-app.on('ready', createWindow);
 
-app.on('window-all-closed', () => {
-    app.quit();
-});
+exports.ui=new AppUI();
+let ui1=new AppUI()
+ui1.drawWindow("img1.jpg");
