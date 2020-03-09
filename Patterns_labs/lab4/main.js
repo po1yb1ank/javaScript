fc569cedef7668d9f0a78be7c41e44c9dea2575a
Bor-is-luv <zverkii5@gmail.com>
2020-03-10 02:08:26 +0300

    delete old version

--- Patterns_labs/lab4/main.js
+++ Patterns_labs/lab4/main.js
@@ -1,19 +1,56 @@
-const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
+const { app, BrowserWindow, nativeImage, globalShortcut } = require('electron');
+
+//let win;
+
+class AppUI {
+    constructor() { };
+
+    drawWindow(pic) {
+        function createWindow() {
+            this.img = nativeImage.createFromPath(pic);
+            let size = this.img.getSize();
+            this.win = new BrowserWindow({
+                width: size.width,
+                height: size.height
+            });
+
+            this.win.loadURL(url.format({
+                pathname: path.join(__dirname, 'main.html'),
+                protocol: 'file:',
+                slashes: true
+            }));
+
+            this.win.on('closed', () => {
+                this.win = null;
+            });
+        }
+
+        app.on('ready', createWindow);
+
+        app.on('window-all-closed', () => {
+            app.quit();
+        }
+        )
+    }
+
+    changeBGImage(img){
+        document.addEventListener('mousedown', function(event) {
+            if (event.button == 2) {
+            document.body.style.backgroundImage = "url(" + img + ")";
+    }});
+    }
+
+    changeBGImage1 = (img) => {
+        document.addEventListener('mousedown', function(event) {
+            if (event.button == 2) {
+            document.body.style.backgroundImage = "url(" + img + ")";
+    }});
+    }
+}
 
 
-let img = new Image();  
-img.src = "/img.jpg";   
-let width = img.width;
-let height = img.height;
-let win;
-function createWindow(){
-    win = new BrowserWindow({width: width, height: height});
-};
-
-win.loadURL(url.format({
-    pathname: path.join(__dirname, 'main.html'),
-    protocol: 'file:',
-    slashes: true
-}));
+exports.ui=new AppUI();
+let ui1=new AppUI()
+ui1.drawWindow("img1.jpg");
